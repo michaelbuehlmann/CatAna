859649e52ff03da84056bae2a8824f5cace389c4
Michael Bühlmann <buehlmann.michi@gmail.com>
2016-02-18 12:16:44 +0100

    Added Analyzer class

---
+++ catana/include/catana/decomposition/Analyzer.hpp
@@ -0,0 +1,30 @@
+//
+// Created by Michael Bühlmann on 18/02/16.
+//
+
+#ifndef CATANA_ANALYZER_HPP
+#define CATANA_ANALYZER_HPP
+
+#include "../iotools.hpp"
+#include "../types.hpp"
+
+#include <memory>
+#include <vector>
+
+class Analyzer {
+public:
+    Analyzer() = default;
+    Analyzer(std::shared_ptr<Source> source, double window_volume);
+
+    void set_source(std::shared_ptr<Source> source);
+    void add_filter(std::shared_ptr<Filter> filter);
+
+    KClkk compute_sfb(unsigned short lmax, unsigned short nmax, double rmax, bool verbose);
+    KClkk compute_sfb(unsigned short lmax, unsigned short nmax, double rmax, unsigned int nside, bool verbose);
+private:
+    std::shared_ptr<Source> source;
+    std::vector<std::shared_ptr<Filter>> filters;
+    double window_volume;
+};
+
+#endif //CATANA_ANALYZER_HPP
---
+++ catana/src/decomposition/Analyzer.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Michael Bühlmann on 18/02/16.
+//
+
+#include <catana/decomposition/Analyzer.hpp>
+#include <catana/decomposition/sfb_decomposition.hpp>
+
+Analyzer::Analyzer(std::shared_ptr<Source> source, double window_volume)
+    : source(source), window_volume(window_volume)
+{}
+
+void Analyzer::set_source(std::shared_ptr<Source> source)
+{
+    this->source = source;
+}
+
+void Analyzer::add_filter(std::shared_ptr<Filter> filter)
+{
+    filters.push_back(filter);
+}
+
+KClkk Analyzer::compute_sfb(unsigned short lmax, unsigned short nmax, double rmax, bool verbose)
+{
+    ObjectContainer oc;
+    {
+        ObjectContainerSink sink(oc);
+        FilterStream fs(source.get(), &sink);
+        for (auto& filter: filters) {
+            fs.add_filter(filter.get());
+        }
+        fs.run();
+    }
+    return decomp_SFB(oc, lmax, nmax, rmax, window_volume, verbose, true);
+}
+
+KClkk Analyzer::compute_sfb(unsigned short lmax, unsigned short nmax, double rmax, unsigned int nside, bool verbose)
+{
+    PixelizedObjectContainer pix_oc(nside);
+    {
+        PixelizedObjectContainerSink sink(pix_oc);
+        FilterStream fs(source.get(), &sink);
+        for (auto& filter: filters) {
+            fs.add_filter(filter.get());
+        }
+        fs.run();
+    }
+    return decomp_SFB(pix_oc, lmax, nmax, rmax, window_volume, verbose, true);
+}
