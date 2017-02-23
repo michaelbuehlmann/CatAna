50f3a8e0760bac08fb97a90f475a1a27cc5c191e
Michael Bühlmann <buehlmann.michi@gmail.com>
2017-02-23 16:07:11 +0100

    Fixed some imports for CLANG compiler

--- python/CatAna/__init__.py
+++ python/CatAna/__init__.py
@@ -2,3 +2,17 @@ from .basictypes import *
 from . import io
 from . import besseltools
 from . import decomposition
+
+
+def init_random(seed=None):
+    """Sets the random seed for the io and decomposition module (used for filtering) 
+    
+    :param seed: The random seed. If None, will be drawn from a random device.
+    :type seed: unsigned integer
+    """
+    if seed is None:
+        decomposition.init_random()
+        io.init_random()
+    else:
+        decomposition.init_random(seed)
+        io.init_random(seed)
